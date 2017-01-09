4939b87741f1938f58d20509fbe861b8c143907f
Svegin <svegin@Svegins-Mac-mini.local>
2017-01-09 17:23:45 -0600

    add display fun

--- master.js
+++ master.js
@@ -1,3 +1,26 @@
-  const addItems = document.querySelector('.add-items');
-  const itemsList = document.querySelector('.plates');
-  const items = [];
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
+
+function addItem(e) {
+  e.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text,
+    done: false
+  }
+  items.push(item);
+  populateList(items, itemsList);
+  this.reset();
+}
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((plate, i) =>{
+  return `
+  <li>
+    <label htmlFor="">${plate.text}</label>
+  </li>
+  `}).join('');
+}
+
+addItems.addEventListener('submit', addItem);
